c40b474e3fc53c1f5c5fd9790d6a92d41af966e1
saicharan-o <saicharanr227@gmail.com>
2026-01-17 22:26:43 +0530

    Exercise-SVM-Code-Updated

--- E-SVM.py
+++ E-SVM.py
@@ -3,6 +3,6 @@ from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
 l=load_digits()
 plt.gray() 
-for i in range(5):
+for i in range(6,12):
     plt.matshow(l.images[i],cmap='inferno')    
 plt.show()   
