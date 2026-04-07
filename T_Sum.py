262843529d54cb4c97eb2dc5454ac2f2894de028
saicharan-o <saicharanr227@gmail.com>
2026-04-07 19:47:01 +0530

    test: added test cases for two sum

--- T_Sum.py
+++ T_Sum.py
@@ -5,3 +5,8 @@ def two_sum_optimized(nums, target):
         if diff in hashmap:
             return [hashmap[diff],i]
         hashmap[num]=i
+
+
+print(two_sum_optimized([2,0,10,15],10))   
+print(two_sum_optimized([2,10,11,5],20))
+print(two_sum_optimized([3,7,9,20],15)) 
