d1f0c2941de8a51dae0c3c46c1e37c8acfe4f421
saicharan-o <saicharanr227@gmail.com>
2026-04-07 19:43:00 +0530

    feat: added optimized hashmap solution for two sum

--- T_Sum.py
+++ T_Sum.py
@@ -1,7 +1,7 @@
-def two_sum(nums, target):
-    n = len(nums)
-    for i in range(n):
-        for j in range(i+1, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-            
+def two_sum_optimized(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
