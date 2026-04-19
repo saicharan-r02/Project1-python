fedf2047e0b593d92882fdffa5a60faec4734465
saicharan-o <saicharanr227@gmail.com>
2026-04-19 15:33:38 +0530

    Changes in CAR_OHE file

--- CAR_OHE.py
+++ CAR_OHE.py
@@ -1,24 +1,36 @@
 import pandas as pd
+
 d=pd.read_csv("Car_P.csv")
 print(d)
+
 dum=pd.get_dummies(d.CarModel,dtype=int)
 print(dum)
+
 m=pd.concat([d,dum],axis="columns")
 print(m)
+
 f=m.drop(["CarModel"],axis="columns")
 print(f)
+
 f=f.drop(["Mercedez Benz C class"],axis="columns")
 print(f)
+
 X=f.drop(["Sell Price($)"],axis="columns")
 print(X)
+
 y=f.drop(["Mileage" , "Age(yrs)" , "Audi A5" ,"BMW X5"],axis="columns")
 print(y)
+
 from sklearn.linear_model import LinearRegression
+
 md=LinearRegression()
 md.fit(X,y)
 md.predict(X)
+
 print(md.score(X,y))
+
 print(md.feature_names_in_)
+
 print(md.predict(pd.DataFrame([[20000,9,0,1]],columns=['Mileage','Age(yrs)','Audi A5','BMW X5'])))
 print(md.predict(pd.DataFrame([[49300,4,1,0]],columns=['Mileage','Age(yrs)','Audi A5','BMW X5'])))
 print(md.predict(pd.DataFrame([[29438,9,0,0]],columns=['Mileage','Age(yrs)','Audi A5','BMW X5'])))
