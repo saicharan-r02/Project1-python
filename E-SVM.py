19e253d02d337809e5ae9823ecf98d883c2f0a65
saicharan-o <saicharanr227@gmail.com>
2026-04-28 12:35:28 +0530

    changes in E-SVM

--- E-SVM.py
+++ E-SVM.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
+
 l=load_digits()
 plt.gray() 
+
 for i in range(6,12):
     plt.matshow(l.images[i],cmap='inferno')    
 plt.show()    
