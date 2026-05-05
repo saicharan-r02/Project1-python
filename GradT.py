f0188f3d18d04c08d17db454a97864f9c8795067
saicharan-o <saicharanr227@gmail.com>
2026-05-05 14:07:35 +0530

    GradT file

--- GradT.py
+++ GradT.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 def gradient_descent(x, y):
     m_curr = 0
     b_curr = 0
@@ -12,9 +13,11 @@ def gradient_descent(x, y):
         m_curr -= rate * md
         b_curr -= rate * bd
     return m_curr, b_curr
+
 x = np.array([1,2,3,4,5])
 y = np.array([5,7,9,11,13])
 m, b = gradient_descent(x, y)
+
 y_predicted = m * x + b
 plt.scatter(x, y, color='blue', marker='+')
 plt.plot(x, y_predicted, color='red')
