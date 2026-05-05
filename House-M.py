2afa1aeee3c17bc01dd59f43aebd6a85033784c5
saicharan-o <saicharanr227@gmail.com>
2026-05-05 14:10:46 +0530

    House file lines added

--- House-M.py
+++ House-M.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
+
 d=pd.read_csv("House-price-M.csv")
 print(d)
 print(d.bedrooms.median())
+
 d.bedrooms=d.bedrooms.fillna(d.bedrooms.median())
 print(d)
+
 reg=linear_model.LinearRegression()
 reg.fit(d.drop("price",axis="columns"),d.price)
 print(reg.coef_)
